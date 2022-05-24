f80364abc98ebf137d9a632e9b5029d1aeeeab35
Patrick <patrick.lopdrup@gmail.com>
2022-05-24 21:40:50 +0200

    Add: save rates and resetbutton

--- contentscript.js
+++ contentscript.js
@@ -1,47 +1,53 @@
 
-
-getRates().then(rates => {
-    console.log(rates.maxRate)
-})
-
 // Get iFrame
 var iframe = document.getElementById('kplayer_ifp')
 
 // Wait for iFrame to load
 iframe.addEventListener("load", function() {
-    var videos = iframe.contentWindow.document.getElementsByTagName('video')
-    var controls = iframe.contentWindow.document.getElementsByClassName('controlsContainer')
+    chrome.storage.sync.get(['startRate', 'maxRate', 'minRate', 'stepRate'], (data) => {
+        var startRate = data.startRate ?? defaultStartRate
+        var maxRate = data.maxRate ?? defaultMaxRate
+        var minRate = data.minRate ?? defaultMinRate
+        var stepRate = data.stepRate ?? defaultStepRate
 
-    var myDiv = makeDiv()
-    var num = makeNumber()
-    var slider = makeSlider()
-    myDiv.appendChild(num)
-    myDiv.appendChild(slider)
+        console.log('startRate', startRate)
 
-    // Make scroll wheel change the playback rate
-    slider.addEventListener('wheel', function(e) {
-        if (e.deltaY < 0) {
-            slider.valueAsNumber += 0.1
-        } else {
-            slider.value -= 0.1
+        var videos = iframe.contentWindow.document.getElementsByTagName('video')
+        var controls = iframe.contentWindow.document.getElementsByClassName('controlsContainer')
+
+        var myDiv = makeDiv()
+        var num = makeNumber(startRate)
+        var slider = makeSlider(minRate, maxRate, stepRate, startRate)
+        myDiv.appendChild(num)
+        myDiv.appendChild(slider)
+
+        // Make scroll wheel change the playback rate
+        slider.addEventListener('wheel', function(e) {
+            stepRate = parseFloat(stepRate)
+            if (e.deltaY < 0) {
+                slider.valueAsNumber += stepRate
+            } else {
+                slider.value -= stepRate
+            }
+            e.preventDefault()
+            e.stopPropagation()
+        })
+
+        // Bind slider to number
+        slider.oninput = function() {
+            updateNumber(num, slider, videos)
         }
-        e.preventDefault()
-        e.stopPropagation()
+        slider.onwheel = function() {
+            updateNumber(num, slider, videos)
+        }
+
+        // Add div to controls
+        controls[0].appendChild(myDiv)
+
+        setPlaybackRate(videos, startRate)
     })
-
-    // Bind slider to number
-    slider.oninput = function() {
-        updateNumber(num, slider, videos)
-    }
-    slider.onwheel = function() {
-        updateNumber(num, slider, videos)
-    }
-
-    // Add div to controls
-    controls[0].appendChild(myDiv)
-
-    setPlaybackRate(videos, 1)
 });
+
 
 
 function updateNumber(num, slider, videos) {
@@ -63,39 +69,25 @@ function makeDiv() {
     return myDiv
 }
 
-function makeNumber() {
+function makeNumber(startRate) {
     var num = document.createElement('span')
-    num.innerHTML = '1'
+    num.innerHTML = startRate
     num.id = 'playbackRateNumber'
     num.style.marginRight = '5px'
     num.style.verticalAlign = 'middle'
     return num
 }
 
-function makeSlider() {
+function makeSlider(min, max, step, value) {
     var slider = document.createElement('input')
     slider.type = 'range'
     slider.id = 'playbackRateSlider'
-    slider.min = 0.2
-    slider.max = 3
-    slider.step = 0.1
-    slider.value = 1
+    slider.min = min
+    slider.max = max
+    slider.step = step
+    slider.value = value
     slider.style.accentColor = '#991100' // DTU red
     slider.style.width = '70px'
     slider.style.verticalAlign = 'middle'
     return slider
-}
-
-async function getRates() {
-    return new Promise((resolve, reject) => {
-        chrome.storage.sync.get(['startRate', 'maxRate', 'minRate', 'stepRate'], resolve)    
-    })
-    .then(data => {
-        return {
-            startRate: data.startRate ?? defaultStartRate,
-            maxRate: data.maxRate ?? defaultMaxRate,
-            minRate: data.minRate ?? defaultMinRate,
-            stepRate: data.stepRate ?? defaultStepRate
-        }
-    })
 }
--- options.js
+++ options.js
@@ -2,6 +2,10 @@
 // Update default values on form submit
 let form = document.forms['defaultRatesForm']
 form.addEventListener('submit', () => {
+	saveFormRates()
+})
+
+function saveFormRates() {
 	let startRate = form['startRate'].value
 	let maxRate = form['maxRate'].value
 	let minRate = form['minRate'].value
@@ -13,15 +17,61 @@ form.addEventListener('submit', () => {
 		'minRate': minRate,
 		'stepRate': stepRate
 	})
-})
+}
 
 setDefaultRatesOnForm()
 
 function setDefaultRatesOnForm() {
 	chrome.storage.sync.get(['startRate', 'maxRate', 'minRate', 'stepRate'], (data) => {
-		form['startRate'].value = data.startRate ?? defaultStartRate
-		form['maxRate'].value = data.maxRate ?? defaultMaxRate
-		form['minRate'].value = data.minRate ?? defaultMinRate
-		form['stepRate'].value = data.stepRate ?? defaultStepRate
+		const isEmpty = Object.keys(data).length === 0
+		if (!isEmpty) {
+			form['startRate'].value = data.startRate
+			form['maxRate'].value = data.maxRate
+			form['minRate'].value = data.minRate
+			form['stepRate'].value = data.stepRate
+		}
 	})
 }
+
+
+form.addEventListener('submit', (e) => {
+	validateForm()
+})
+
+function validateForm() {
+	let startRate = form['startRate'].value
+	let maxRate = form['maxRate'].value
+	let minRate = form['minRate'].value
+	let stepRate = form['stepRate'].value
+
+	if (startRate < minRate) {
+		alert(ErrorMessage['START_RATE_TOO_LOW'])
+		return false
+	}
+	if (startRate > maxRate) {
+		alert(ErrorMessage['START_RATE_TOO_HIGH'])
+		return false
+	}
+	if (minRate > maxRate) {
+		alert(ErrorMessage['MIN_RATE_TOO_HIGH'])
+		return false
+	}
+	return true
+}
+
+const ErrorMessage = {
+	'START_RATE_TOO_LOW': 'Start speed must be greater than min speed',
+	'START_RATE_TOO_HIGH': 'Start speed must be less than max speed',
+	'MIN_RATE_TOO_HIGH': 'Min speed must be less than max speed',
+	'INVALID_RATES': 'Invalid rates'
+}
+
+var resetButton = form['resetButton']
+resetButton.addEventListener('click', () => {
+	form['startRate'].value = 1
+	form['maxRate'].value = 3
+	form['minRate'].value = 0.2
+	form['stepRate'].value = 0.1
+
+	saveFormRates()
+})
